46e7ac4658e617dc20bbc1d80d1a038efe77dd7c
ch3n9w <ch3n9w@gmail.com>
2023-08-19 19:46:21 +0800

    update

--- hypr/bin/scratchpad.sh
+++ hypr/bin/scratchpad.sh
@@ -2,7 +2,6 @@
 
 hasspecial=$(hyprctl clients -j | jq -r '.[] | select(.workspace.name=="special:scratchpad")')
 if [ -z "$hasspecial" ]; then
-  # /home/ch4ser/.local/bin/kitty --class "scratchpad" &
   kitty --class "scratchpad" &
   sleep 0.5
 fi

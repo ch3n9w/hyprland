fde3b329744bd7cb7d01006bd018f01a9ee68419
ch3n9w <ch3n9w@gmail.com>
2023-08-24 23:13:55 +0800

    replace kitty with alacritty for ssh compatibility

--- hypr/bin/scratchpad.sh
+++ hypr/bin/scratchpad.sh
@@ -2,7 +2,7 @@
 
 hasspecial=$(hyprctl clients -j | jq -r '.[] | select(.workspace.name=="special:scratchpad")')
 if [ -z "$hasspecial" ]; then
-  kitty --class "scratchpad" &
+  alacritty --class "scratchpad" &
   sleep 0.5
 fi
 hyprctl dispatch togglespecialworkspace scratchpad 
--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@ rm -r hypr waybar rofi dunst swaylock applications
 cp -r ~/.config/hypr ./
 cp -r ~/.config/swaylock ./
 cp -r ~/.config/waybar ./
-rm ./waybar/bin/forest
 cp -r ~/.config/rofi ./
 cp -r ~/.config/dunst ./
 cp -r ~/.local/share/applications ./

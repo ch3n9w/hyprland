9da527cdbf8c208d2f7b6c5034d725c359ffc48c
ch3n9w <ch3n9w@gmail.com>
2023-09-30 20:59:39 +0800

    fix hyprland special workspace

--- hypr/bin/scratchpad.sh
+++ hypr/bin/scratchpad.sh
@@ -2,7 +2,6 @@
 
 hasspecial=$(hyprctl clients -j | jq -r '.[] | select(.workspace.name=="special")')
 if [ -z "$hasspecial" ]; then
-  # /home/ch4ser/.local/bin/kitty --class "scratchpad" &
   alacritty --class "scratchpad" &
   sleep 0.5
 else

8ce98f580f44e3978384cfabede0bc7e9eac6138
ch3n9w <ch4xer@gmail.com>
2023-10-04 15:33:02 +0800

    use kitty instead of alacritty

--- hypr/bin/scratchpad.sh
+++ hypr/bin/scratchpad.sh
@@ -2,7 +2,7 @@
 
 hasspecial=$(hyprctl clients -j | jq -r '.[] | select(.workspace.name=="special")')
 if [ -z "$hasspecial" ]; then
-  alacritty --class "scratchpad" &
+  kitty --class "scratchpad" &
   sleep 0.5
 else
   hyprctl dispatch togglespecialworkspace

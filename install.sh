af2bb10567ec0bbe88a439c2334a9895bacc825d
ch3n9w <ch4xer@gmail.com>
2023-12-05 17:25:19 +0800

    update

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@ cp -r dunst ~/.config/
 cp -r rofi ~/.config/
 cp -r swaylock ~/.config/
 cp -r waybar ~/.config/
-cp -r applications ~/.local/share/
 
 
--- update.sh
+++ update.sh
@@ -4,4 +4,3 @@ cp -r ~/.config/swaylock ./
 cp -r ~/.config/waybar ./
 cp -r ~/.config/rofi ./
 cp -r ~/.config/dunst ./
-cp -r ~/.local/share/applications ./
